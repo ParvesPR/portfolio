f9760612e2c904105776250c20c96a208a1c4316
ParvesPR <parves2585@gmail.com>
2022-06-05 14:05:00 +0800

    header added

---
+++ src/components/Shared/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className='ms-auto '>
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/service">Service</Nav.Link>
+                        <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
